eb177a684385c7fcdaf393ee71fab2e5e249ea0b
AMIT YADAV <AMIT YADAV@AMITYADAV-PC>
2020-03-19 22:06:30 +0530

    find the length of string

--- JobWallet/src/Demo.java
+++ JobWallet/src/Demo.java
@@ -3,6 +3,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String s="New String";
+		System.out.println(s.length());
 		System.out.println("hi this my first program");
 	}
 
