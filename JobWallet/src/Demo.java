1630e4a98f8dd8373841a411e6728709668922ab
AMIT YADAV <AMIT YADAV@AMITYADAV-PC>
2020-03-19 22:14:30 +0530

    reverse of the string

--- JobWallet/src/Demo.java
+++ JobWallet/src/Demo.java
@@ -7,6 +7,13 @@ public class Demo {
 		String s="New String";
 		System.out.println(s.length());
 		System.out.println("hi this my first program");
+		
+		
+		StringBuffer sb=new StringBuffer();
+		sb.append(s);
+		sb=sb.reverse();
+		System.out.println(sb);
+		
 	}
 
 }
